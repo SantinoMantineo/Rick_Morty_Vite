565c8809b8df5d8277ee9686d714cf2c3871a037
Santino Mantineo <santinomantineo@gmail.com>
2023-09-07 15:21:11 -0300

    New About

--- Client/src/Components/About/About.jsx
+++ Client/src/Components/About/About.jsx
@@ -7,28 +7,31 @@ import Github from '../../assets/github.png'
 
 export default function About() {
   return (
-    <>
     <div className={style.aboutContainer}>
       <h2>ABOUT ME</h2>
-      <div className={style.perfil}>
-        <img src={Perfil}></img>
-      </div>
-      <div className={style.text}>
-        <p>
-          Mi nombre es Maximiliano Valli, tengo 38 años y soy de la provincia de Santa Fe,<br></br>
-          empecé este año a estudiar en Henry porque tenía ganas de aprender
-          programación.<br></br>
-          <br></br>
-          Hasta ahora la expericia es genial y esta página fue construída con
-          todo lo que aprendí.
-        </p>
-        <div>
-          <a href='https://www.instagram.com/maxi.valli.aicardi/' target="_blank"> <img src={Instagram} alt="instagram" className={style.logo}/> </a>
-          <a href='mailto:maximilianovalli.sc@gmail.com'> <img src={Gmail} alt="gmail" className={style.logo}/> </a>
-          <a href='https://github.com/maxivalli' target="_blank"> <img src={Github} alt="github" className={style.logo}/> </a>
+      <div className={style.grid}>
+        <div className={style.aboutImg}>
+          <img src={Perfil}></img>
+        </div>
+        <div className={style.aboutP}>
+          <h2>¿Quien Soy?</h2>
+          <p className={style.P}>
+            I am a passionate designer and front-end
+            programmer dedicated to crafting visually 
+            stunning and user-friendly interfaces.
+            <br></br>
+            <br></br>
+            I am highly motivated to expand 
+            my skills and stay current with 
+            the ever-evolving features and technologies.
+          </p>
         </div>
       </div>
-    </div>
-    </>
+      <div>
+          {/* <a href='https://www.instagram.com/maxi.valli.aicardi/' target="_blank"> <img src={Instagram} alt="instagram" className={style.logo}/> </a>
+          <a href='mailto:maximilianovalli.sc@gmail.com'> <img src={Gmail} alt="gmail" className={style.logo}/> </a>
+          <a href='https://github.com/maxivalli' target="_blank"> <img src={Github} alt="github" className={style.logo}/> </a> */}
+        </div>
+      </div>
   );
 }
