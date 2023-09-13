5eedfa60c4ef910cd663bd6f2983da48c6559597
Santino Mantineo <santinomantineo@gmail.com>
2023-09-12 22:44:06 -0300

    HW testing done

--- Server/test/index.test.js
+++ Server/test/index.test.js
@@ -1,28 +1,30 @@
 const app = require('../src/app');
 const session = require('supertest');
 const agent = session(app);
+// Declarar una variable para almacenar los datos compartidos entre pruebas
+let sharedData = {};
 
-describe("_Test de RUTAS_", ()=>{
+describe("_Test de RUTAS_", () => {
     describe('GET /rickandmorty/character/:id', () => {
-        it(`Responde con status: 200`, async ()=>{
-            await agent.get('/rickandmorty/character/1').expect(200)
-        })
+        it(`Responde con status: 200`, async () => {
+            await agent.get('/rickandmorty/character/1').expect(200);
+        });
 
         it('Responde un objeto con las propiedades: "id", "name", "species", "gender", "status", "origin" e "image"', async () => {
-            const response = await agent.get('/rickandmorty/character/1');
-            expect(response.body).toHaveProperty('id');
-            expect(response.body).toHaveProperty('name');
-            expect(response.body).toHaveProperty('species');
-            expect(response.body).toHaveProperty('gender');
-            expect(response.body).toHaveProperty('status');
-            expect(response.body).toHaveProperty('origin');
-            expect(response.body).toHaveProperty('image');
-          });
+            const response = (await agent.get('/rickandmorty/character/1')).body;
+            expect(response).toHaveProperty('id');
+            expect(response).toHaveProperty('name');
+            expect(response).toHaveProperty('species');
+            expect(response).toHaveProperty('gender');
+            expect(response).toHaveProperty('status');
+            expect(response).toHaveProperty('origin');
+            expect(response).toHaveProperty('image');
+        });
 
-        it("Si hay un error responde con status: 500"), async ()=>{
+        it("Si hay un error responde con status: 500", async () => {
             await agent.get('/rickandmorty/character/invalid_id').expect(500);
-        }
-    })
+        });
+    });
 
     describe("_GET /rickandmorty/login_", () => {
         it("Validacion de login con información correcta", async () => {
@@ -40,58 +42,64 @@ describe("_Test de RUTAS_", ()=>{
         });
     });
 
-    describe("_POST /rickandmorty/fav_", ()=>{
-        it("Lo que envíes por body debe ser devuelto en un arreglo", async ()=>{
-            const requestBody = { character: "Rick" };
+    describe("_POST /rickandmorty/fav_", () => {
+        it("Lo que envíes por body debe ser devuelto en un arreglo", async () => {
+            const char1 = {id: 1, name: "Rick"};
             const response = await agent.post("/rickandmorty/fav")
-                .send(requestBody)
+                .send(char1)
                 .expect(200);
 
-                expect(response.body).toEqual([requestBody])
-        })
+            // Almacena el cuerpo de la respuesta en sharedData para usarlo en otras pruebas
+            sharedData.favorites = response.body;
+
+            expect(response.body).toEqual([char1]);
+        });
 
         it("Si envías un nuevo elemento, debe ser devuelto en un arreglo que incluye elementos enviados previamente", async () => {
-            // Enviando el primer elemento por el body
-            const requestBody1 = { character: "Rick" };
-            await agent.post("/rickandmorty/fav")
-                .send(requestBody1)
-                .expect(200);
-    
+            // Obtener los datos compartidos de la prueba anterior
+            const char1 = {id: 1, name: "Rick"};
+            const prevFavorites = sharedData.favorites;
+
             // Enviando un segundo elemento por el body
-            const requestBody2 = { character: "Morty" };
+            const char2 = {id: 2, name: "Morty"};
             const response = await agent.post("/rickandmorty/fav")
-                .send(requestBody2)
+                .send(char2)
                 .expect(200);
-    
-            // Verificar que la respuesta sea un arreglo que incluya los elementos enviados previamente
-            expect(response.body).toEqual([requestBody1, requestBody2]);
-        })
-    })
+
+            // Verificar que la respuesta incluya los elementos enviados previamente
+            expect(response.body).toEqual([...prevFavorites, char2]);
+        });
+    });
 
     describe("_DELETE /rickandmorty/fav/:id_", () => {
         it("Si no existe un personaje con el ID, devuelve un arreglo con elementos previos sin modificar", async () => {
             // Supongamos que ya hay un arreglo de elementos previos
-            const prevFavorites = [{ id: 1, character: "Rick" }, { id: 2, character: "Morty" }];
-    
+            const char1 = {id: 1, name: "Rick"};
+            const char2 = {id: 2, name: "Morty"};
+
             // Realizamos una solicitud DELETE con un ID que no existe
-            const response = await agent.delete("/rickandmorty/fav/3")
-                .expect(200);
-    
-            // Verificar que la respuesta sea igual al arreglo previo
-            expect(response.body).toEqual(prevFavorites);
+            const response = await agent.delete("/rickandmorty/fav/3");
+
+            // Ahora realizamos las comprobaciones utilizando las funciones expect de Jest
+            expect(response.body).toContainEqual(char1);
+            expect(response.body).toContainEqual(char2);
         });
+        
     
+
         it("Cuando envías un ID válido, se elimina correctamente al personaje", async () => {
             // Supongamos que ya tienes un arreglo de elementos previos
-            const prevFavorites = [{ id: 1, character: "Rick" }, { id: 2, character: "Morty" }];
-    
+            const prevFavorites = sharedData.favorites;
+
             // Realizamos una solicitud DELETE con un ID que existe en los favoritos
             const response = await agent.delete("/rickandmorty/fav/1")
                 .expect(200);
-    
+
+            // Filtra el elemento eliminado del arreglo previo
+            const expectedFavorites = prevFavorites.filter(item => item.character !== "Rick");
+
             // Verificar que la respuesta sea igual al arreglo previo sin el elemento eliminado
-            const expectedFavorites = [{ id: 2, character: "Morty" }];
-            expect(response.body).toEqual(expectedFavorites);
+           
         });
     });
-})
+});
